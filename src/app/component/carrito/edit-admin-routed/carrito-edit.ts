d4df1fe19f9e0f815f6e8aeadc3f45045cc12dd9
“rond88” <r0nald19202006@gmail.com>
2026-02-09 18:14:10 +0100

    carrito edit empezado

---
+++ src/app/component/carrito/edit-admin-routed/carrito-edit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-admin-routed',
+  imports: [],
+  templateUrl: './carrito-edit.html',
+  styleUrl: './carrito-edit.css',
+})
+export class EditAdminRouted {
+
+}
